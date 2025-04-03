00683bd3263084938c8fe634b96700f1278e9e6b
Grispys <grispanimation@gmail.com>
2025-04-03 15:28:45 -0230

    removed old comments
    added new previous button async function that calls updateHTML with a new argument
    
    added arguments to updateHTML and getBackend, so different post requests are made depending on whether the frontend wants all trees or just the most recent one

--- utils/hookups.js
+++ utils/hookups.js
@@ -8,41 +8,58 @@ let numbers;
 
 let networkConnection;
 
+// async button function that turns the user string input into a number array for the backend
 document.getElementById("submit").onclick = async function (){
     answer = document.getElementById("answer").value
     let validity = /^[0-9,]+$/.test(answer)
     if(validity){
         numbers = answer.split(',').map(Number);
-        // numbers.sort(function(a,b) {
-            // return a - b;
-        // });
-        // gTLNumbers = numbers.toReversed(); 
         console.log(numbers)
-        await updateHTML()
+        await updateHTML("recent")
     }else{
         window.alert(`Please enter a query that follows the format "1,2,3,4,5..."`)
-    }
-
-    
-    
+    }  
 }
 
+document.getElementById("previous").onclick = async function (){
+    await updateHTML("all")
+}  
+
+
+
 // updates the html element "getrequest", which is a p tag, to the getRequests data
-async function updateHTML(){
+async function updateHTML(query){
     var element = document.getElementById("getRequest");
     element.innerHTML = "Loading..."
-    await postBackend()
     
-    await getBackend()
-    if(networkConnection){
-        console.log(getData)
-        var parsedData = JSON.parse(getData);
-        var indentedData = JSON.stringify(parsedData, null, 2);
-        element.innerHTML = `<pre>${indentedData}</pre>`;
+    if(query =="recent"){
+        await postBackend()
+    
+        await getBackend("recent")
+        if(networkConnection){
+            console.log(getData)
+            var parsedData = JSON.parse(getData);
+            var indentedData = JSON.stringify(parsedData, null, 2);
+            element.innerHTML = `<pre>${indentedData}</pre>`;
+            
+        }else{
+            element.innerHTML = "Ooops! Looks like you don't have the backend set up. <br> I'm looking at http://localhost:8080/api/trees. Is anything there?"
+        }
+    }else if(query =="all"){
         
-    }else{
-        element.innerHTML = "Ooops! Looks like you don't have the backend set up. <br> I'm looking at http://localhost:8080/api/trees. Is anything there?"
+        await getBackend("all")
+        if(networkConnection){
+            console.log(getData)
+            var parsedData = JSON.parse(getData);
+            var indentedData = JSON.stringify(parsedData, null, 2);
+            element.innerHTML = `<pre>${indentedData}</pre>`;
+            
+        }else{
+            element.innerHTML = "Ooops! Looks like you don't have the backend set up. <br> I'm looking at http://localhost:8080/api/trees. Is anything there?"
+        }
     }
+
+   
    
 }
 
@@ -66,9 +83,16 @@ async function postBackend(){
 
 // sends a get request to the localhost, changes it to string, and sets global variable
 // getData to that stringified jumbo
-async function getBackend(){
+async function getBackend(query){
+    let fetchURL;
+    if(query == "recent"){
+        fetchURL = 'http://localhost:8080/api/trees/most-recent'
+    }else if(query == "all"){
+        fetchURL = 'http://localhost:8080/api/trees/previous-trees'
+    }
+
     try {
-		const response = await fetch('http://localhost:8080/api/trees/most-recent');
+		const response = await fetch(fetchURL);
 		networkConnection = true;
 		const rawData = await response.json();
 		getData = rawData;
